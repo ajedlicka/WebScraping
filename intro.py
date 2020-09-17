ee093ca864130cf5a237ada8b5768a1570f616e5
Andy Jedlicka <andyjedlicka@Andys-MacBook-Pro.local>
2020-09-17 19:09:40 -0400

    command line arguments added to web scraping

--- intro.py
+++ intro.py
@@ -1,1 +1,15 @@
+# web scraping library
 import bs4
+
+# getting command line arguments
+import sys
+
+# getting command line arguments
+args = sys.argv
+
+# assigning command line arguments to variables for search
+business_type = args[1]
+location = args[2]
+
+# creating url with command line arguments
+url = "https://www.yelp.com/search?find_desc=" + business_type + "&find_loc=" + location
